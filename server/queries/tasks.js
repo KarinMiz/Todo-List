a9eb18ea118c4d0472a2e07f1a6d5bdd7db9dc64
KarinMiz <karinmizrhahi@gmail.com>
2023-03-27 10:04:56 +0300

    update addTaskQuery

--- server/queries/tasks.js
+++ server/queries/tasks.js
@@ -1,7 +1,7 @@
 
 const getAllTasksQuery = "SELECT * FROM tasks";
 
-const addTaskQuery = "INSERT INTO tasks (`title`,`category_id`,`description`, `isdone`, `deadline`) VALUES (?)";
+const addTaskQuery = "INSERT INTO tasks(title,category_id,description, isdone, deadline) VALUES (?)";
 
 const getTaskQuery = "SELECT * FROM tasks WHERE task_id = $1"
 
